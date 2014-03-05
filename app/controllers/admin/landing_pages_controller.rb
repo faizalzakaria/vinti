9f28ae19c2f85c409bfb7098ff89de62854fea2f
Faizal Zakaria <phaibusiness@gmail.com>
2014-03-05 20:24:47 +0800

    Add admin to show all workouts

--- app/controllers/admin/landing_pages_controller.rb
+++ app/controllers/admin/landing_pages_controller.rb
@@ -1,6 +1,11 @@
 class Admin::LandingPagesController < ApplicationController
 
   def index
+    @workouts = Workout.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @workouts }
+    end
   end
 
 end
