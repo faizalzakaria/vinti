a708c395595f63a434e380265590c5b181c884f5
Faizal Zakaria <phaibusiness@gmail.com>
2014-03-10 19:35:38 +0800

    Show all star

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -2,6 +2,7 @@ class RankController < ApplicationController
 
   def index
     @challenges = Challenge.all
+    @stars = User.order("workout_distance DESC")
     respond_to do |format|
       format.html
       format.json { render jsong: @ranking }
